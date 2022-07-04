828077b9768ea186ac8c0bf68c828fefdd2e03ce
Arhant Bararia <arhantbararia@gmail.com>
2022-07-04 14:17:21 +0530

    Hashtable DS added

---
+++ Hashtable.cpp
@@ -0,0 +1,16 @@
+class Hashtable {
+    std::unordered_map<const void *, const void *> htmap;
+
+public:
+    void put(const void *key, const void *value) {
+            htmap[key] = value;
+    }
+
+    const void *get(const void *key) {
+            return htmap[key];
+    }
+
+};
+// smart and flexible hashtable data structure
+
+
